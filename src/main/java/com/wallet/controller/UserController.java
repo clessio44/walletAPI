83074d57e2b8d149671033ee329977b9429891f4
Clessio <clessiosilva44@gmail.com>
2024-01-11 10:46:16 -0300

    atualizando testes

--- src/main/java/com/wallet/controller/UserController.java
+++ src/main/java/com/wallet/controller/UserController.java
@@ -2,6 +2,7 @@ package com.wallet.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,6 +30,10 @@ public class UserController {
 		
 		Response<UserDTO> response = new Response<UserDTO>();
 		
+		if(result.hasErrors()) {
+			result.getAllErrors().forEach(e -> response.getErrors().add(e.getDefaultMessage()));
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+		}
 		User user = service.save(this.convertDtoToEntity(dto));
 		response.setData(this.convertEntityToDto(user));
 		
@@ -36,6 +41,7 @@ public class UserController {
 	}
 	private User convertDtoToEntity(UserDTO dto) {
 		User u = new User();
+		u.setId(dto.getId());
 		u.setEmail(dto.getEmail());
 		u.setName(dto.getName());
 		u.setPassword(dto.getPassword());
@@ -43,9 +49,10 @@ public class UserController {
 	}
 	private UserDTO convertEntityToDto(User u) {
 		UserDTO dto = new UserDTO();
-		dto.setEmail(dto.getEmail());
-		dto.setName(dto.getName());
-		dto.setPassword(dto.getPassword());
+		dto.setId(u.getId());
+		dto.setEmail(u.getEmail());
+		dto.setName(u.getName());
+		dto.setPassword(u.getPassword());
 		return dto;
 	}
 }
